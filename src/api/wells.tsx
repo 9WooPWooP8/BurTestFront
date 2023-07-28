fe526a70fdc7cd8a9706ff063f7afede5b3a86ed
Niktia Bykov <kilops.com@gmail.com>
2023-07-28 09:12:38 +0500

    changed well api route

--- src/api/wells.tsx
+++ src/api/wells.tsx
@@ -1,7 +1,7 @@
 import { Well } from "./types";
 
 export const getWells = async (): Promise<Array<Well>>  => {
-	const response = await fetch('http://localhost:5055/Well/detailed/active');
+	const response = await fetch('http://localhost:5055/Well/detailed');
 	let json = response.json();
 	return json
 }
